45b20fcd233864752789f3432b1d3a895df32fd9
Rodolfo Roman <rromanv@gmail.com>
2025-03-21 17:12:52 -0500

    fix: update test descriptions for Two Sum problem

--- test/1.spec.ts
+++ test/1.spec.ts
@@ -1,8 +1,14 @@
 import { expect, test } from 'vitest'
 import { twoSum } from '@/1'
 
-test('1. Two Sum', () => {
+test('nums = [2, 7, 11, 15], target = 9 => [0, 1]', () => {
   expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1])
+})
+
+test('nums = [3, 2, 4], target = 6 => [1, 2]', () => {
   expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2])
+})
+
+test('nums = [3, 3], target = 6 => [0, 1]', () => {
   expect(twoSum([3, 3], 6)).toStrictEqual([0, 1])
 })
