0ccb55d152ed35dccef8c65efe2f8b7405deadfe
Rodolfo Roman <rromanv@gmail.com>
2025-04-27 23:17:57 -0500

    fix: update time and space complexity comments and optimize palindrome check logic

--- src/125.ts
+++ src/125.ts
@@ -6,26 +6,25 @@
  * Determines if a string is a palindrome, considering only alphanumeric characters and ignoring case
  *
  * Algorithm: Two-pointer approach
- * Time Complexity: O(n) where n is the length of the string
- * Space Complexity: O(n)
+ * Time Complexity: O(N) where n is the length of the string
+ * Space Complexity: O(1)
  *
  * @param {string} s - The string to check
  * @returns {boolean} - True if the string is a palindrome, false otherwise
  */
 function isPalindrome(s: string): boolean {
-  const justChars = s
-    .trim()
-    .toLowerCase()
-    .replace(/[^0-9a-z]/gi, '')
-
+  const isAlpNum = (char: string) => /[0-9a-z]/i.test(char)
   let left = 0
-  let right = justChars.length - 1
+  let right = s.length - 1
 
   while (left < right) {
-    if (justChars[left] !== justChars[right]) return false
+    while (left < right && !isAlpNum(s[left])) left++
+    while (left < right && !isAlpNum(s[right])) right--
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) return false
     left++
     right--
   }
+
   return true
 }
 
