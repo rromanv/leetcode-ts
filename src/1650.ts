69df74be49350c6ab57291307d93a0606350e79e
Rodolfo Roman <rromanv@gmail.com>
2025-04-28 16:37:03 -0500

    fix: handle null inputs in lowestCommonAncestor function and improve parent traversal logic

--- src/1650.ts
+++ src/1650.ts
@@ -2,6 +2,10 @@
  * 1650. Lowest Common Ancestor of a Binary Search Tree III
  * https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree-iii
  * Medium
+ *
+ * Algorithm: Parent Transversal with Two Pointers
+ * Time Complexity: O(h) where h is the height of the tree
+ * Space Complexity: O(1)
  */
 
 class _Node {
@@ -19,19 +23,12 @@ class _Node {
 }
 
 function lowestCommonAncestor(p: _Node | null, q: _Node | null): _Node | null {
-  if (!p || !q) throw new Error('Two Nodes need to be provided as p and q respectively')
-
-  if (p.val === q.val) return p
-
-  if (!p.parent) return p
-  if (!q.parent) return q
-
   let pNode = p
   let qNode = q
 
   while (pNode !== qNode) {
-    pNode = pNode.parent ?? q
-    qNode = qNode.parent ?? p
+    pNode = pNode?.parent ?? q
+    qNode = qNode?.parent ?? p
   }
 
   return pNode
