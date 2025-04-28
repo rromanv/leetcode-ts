70166ff41b20a59db9925611e5ab7c1ed2414a0c
Rodolfo Roman <rromanv@gmail.com>
2025-04-28 16:24:17 -0500

    refactor: improve variable naming and structure in merge function for clarity

--- src/88.ts
+++ src/88.ts
@@ -4,14 +4,26 @@
  * Easy
  *
  * Do not return anything, modify nums1 in-place instead.
+ *
+ * Algorithm: Reverse merge approach
+ * Time complexity: O(m + n)
+ * Space complexity: O(1)
  */
 
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-  let write = m + n - 1
-  let index1 = m - 1
-  let index2 = n - 1
-  while (index2 >= 0) {
-    nums1[write--] = index1 >= 0 && nums1[index1] > nums2[index2] ? nums1[index1--] : nums2[index2--]
+  let writeIdx = m + n - 1
+  let mIdx = m - 1
+  let nIdx = n - 1
+
+  while (nIdx >= 0) {
+    if (mIdx >= 0 && nums1[mIdx] > nums2[nIdx]) {
+      nums1[writeIdx] = nums1[mIdx]
+      mIdx--
+    } else {
+      nums1[writeIdx] = nums2[nIdx]
+      nIdx--
+    }
+    writeIdx--
   }
 }
 
